ad30cf5aba48d257574606fa717404a5a3fbb0fb
guitarlum <steffen.dangmann@gmail.com>
2026-09-22 00:23:17 +0200

    Bump the 1.3.0 line to 1.3.0 so a test build is not older than shipped v1.2.3

--- NeuralAmpModeler/config.h
+++ NeuralAmpModeler/config.h
@@ -1,7 +1,7 @@
 #define PLUG_NAME "VoLum"
 #define PLUG_MFR "Lum"
-#define PLUG_VERSION_HEX 0x00010203
-#define PLUG_VERSION_STR "1.2.3"
+#define PLUG_VERSION_HEX 0x00010300
+#define PLUG_VERSION_STR "1.3.0"
 #define PLUG_UNIQUE_ID 'VoLm'
 #define PLUG_MFR_ID 'Lum0'
 #define PLUG_URL_STR "https://github.com/guitarlum/VoLum"
